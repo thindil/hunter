3ee16bf8084070cd1f1ab04f3042a0e3f7b135db
thindil@laeran.pl <thindil@laeran.pl>
2019-03-29 17:50:06 +0000

    removed unneeded variable
    FossilOrigin-Name: f933ac2e3a46c641db970b2865e5cac4fa92693e4cf11bc4a61e335b356e4582

--- hunter.sh
+++ hunter.sh
@@ -9,7 +9,6 @@ if [ -d lib ]; then
    export GDK_PIXBUF_MODULE_FILE=$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
    export GDK_PIXBUF_MODULEDIR=$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders/
    export FONTCONFIG_FILE=$prefix/etc/fonts/fonts.conf
-   export XDG_DATA_DIRS=$XDG_DATA_DIRS:$prefix/share
    export GSETTINGS_BACKEND=memory
 else
    eval `gtkada-env.sh --print-only`
