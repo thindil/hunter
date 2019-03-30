84f55ba0b80fffe54ad62240d0217759dd691940
thindil@laeran.pl <thindil@laeran.pl>
2019-03-30 09:31:43 +0000

    update XDG_DATA_DIRS variable if empty
    FossilOrigin-Name: 477d25cdbb30aed59250654e79cf490e01eb03690e9613bf4fe5ea95de70f371

--- hunter.sh
+++ hunter.sh
@@ -5,6 +5,9 @@ cd "$prefix"
 prefix=`pwd`
 
 if [ -d lib ]; then
+   if [ -z XDG_DATA_DIRS ]; then
+      export XDG_DATA_DIRS=/usr/share
+   fi
    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$prefix/lib
    export GDK_PIXBUF_MODULE_FILE=$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
    export GDK_PIXBUF_MODULEDIR=$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders/
