b87e06e083a685760740e3e0763c69bb4e197b0d
thindil@laeran.pl <thindil@laeran.pl>
2019-03-31 08:30:51 +0000

    added listing for directories
    FossilOrigin-Name: 661a50019dbdcb1cd9fd9f95a6541fd86f9fb592fb3dce68f88e201e29dbd4ec

--- src/mainwindow.adb
+++ src/mainwindow.adb
@@ -13,9 +13,14 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+with Ada.Environment_Variables; use Ada.Environment_Variables;
+with Ada.Directories; use Ada.Directories;
 with Gtk.Main;
 with Gtk.Widget; use Gtk.Widget;
 with Gtk.Paned; use Gtk.Paned;
+with Gtk.List_Store; use Gtk.List_Store;
+with Gtk.Tree_Model; use Gtk.Tree_Model;
+with Gtk.Enums; use Gtk.Enums;
 with Glib; use Glib;
 
 package body MainWindow is
@@ -39,12 +44,44 @@ package body MainWindow is
             0.4));
    end ResizePaned;
 
+   procedure LoadDirectory(Name: String) is
+      FilesList: constant Gtk_List_Store :=
+        Gtk_List_Store(Get_Object(Builder, "fileslist"));
+      FileIter: Gtk_Tree_Iter;
+      Files: Search_Type;
+      FoundFile: Directory_Entry_Type;
+   begin
+      FilesList.Clear;
+      Start_Search(Files, Name, "");
+      while More_Entries(Files) loop
+         Get_Next_Entry(Files, FoundFile);
+         Append(FilesList, FileIter);
+         if Kind(FoundFile) = Directory then
+            if Simple_Name(FoundFile)(1) = '.' then
+               Set(FilesList, FileIter, 0, "   " & Simple_Name(FoundFile));
+            else
+               Set(FilesList, FileIter, 0, "  " & Simple_Name(FoundFile));
+            end if;
+         else
+            if Simple_Name(FoundFile)(1) = '.' then
+               Set(FilesList, FileIter, 0, " " & Simple_Name(FoundFile));
+            else
+               Set(FilesList, FileIter, 0, Simple_Name(FoundFile));
+            end if;
+         end if;
+         Set(FilesList, FileIter, 1, "");
+      end loop;
+      End_Search(Files);
+      Set_Sort_Column_Id(FilesList, 0, Sort_Ascending);
+   end LoadDirectory;
+
    procedure CreateMainWindow(NewBuilder: Gtkada_Builder) is
    begin
       Builder := NewBuilder;
       Register_Handler(Builder, "Main_Quit", Quit'Access);
       Register_Handler(Builder, "Resize_Paned", ResizePaned'Access);
       Do_Connect(Builder);
+      LoadDirectory(Value("HOME"));
       Show_All(Gtk_Widget(Get_Object(Builder, "mainwindow")));
    end CreateMainWindow;
 
