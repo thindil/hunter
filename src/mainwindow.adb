7d39bfc589cb3b589614c7e7a515f3528583e4c0
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-04-07 08:45:10 +0000

    use icon-name instead stock-id
    FossilOrigin-Name: 03730a6674e6b5352f2382dfa4202fd85f3d4ae9e743d76783e56dea3f8a476b

--- src/mainwindow.adb
+++ src/mainwindow.adb
@@ -123,7 +123,7 @@ package body MainWindow is
             else
                Set(FilesList, FileIter, 1, 2);
             end if;
-            Set(FilesList, FileIter, 2, "gtk-directory");
+            Set(FilesList, FileIter, 2, "folder");
             if ListName = "fileslist1" then
                goto End_Of_Loop;
             end if;
@@ -154,7 +154,7 @@ package body MainWindow is
             else
                Set(FilesList, FileIter, 1, 4);
             end if;
-            Set(FilesList, FileIter, 2, "gtk-file");
+            Set(FilesList, FileIter, 2, "text-x-generic-template");
             if ListName = "fileslist1" then
                goto End_Of_Loop;
             end if;
