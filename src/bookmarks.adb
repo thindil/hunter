08349ee3cf824838cd60355e83b2b27b289cc850
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-05-02 05:56:41 +0000

    moved bookmarks code to separated package
    FossilOrigin-Name: 9944180821e3e1ec2888d97c56977556cf55f7110e316cc2999c475b72d1f792

---
+++ src/bookmarks.adb
@@ -0,0 +1,46 @@
+-- Copyright (c) 2019 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Directories;
+with Ada.Environment_Variables; use Ada.Environment_Variables;
+with MainWindow; use MainWindow;
+
+package body Bookmarks is
+
+   procedure GoToBookmark(Self: access Gtk_Menu_Item_Record'Class) is
+      MenuLabel: constant Unbounded_String :=
+        To_Unbounded_String(Get_Label(Self));
+   begin
+      for I in BookmarksList.Iterate loop
+         if MenuLabel = BookmarksList(I).MenuName then
+            CurrentDirectory := BookmarksList(I).Path;
+            exit;
+         end if;
+      end loop;
+      if Ada.Directories.Exists(To_String(CurrentDirectory)) then
+         Reload(Builder);
+      end if;
+   end GoToBookmark;
+
+   procedure GoHome(Object: access Gtkada_Builder_Record'Class) is
+      pragma Unreferenced(Object);
+   begin
+      CurrentDirectory := To_Unbounded_String(Value("HOME"));
+      if Ada.Directories.Exists(To_String(CurrentDirectory)) then
+         Reload(Builder);
+      end if;
+   end GoHome;
+
+end Bookmarks;
---
+++ src/bookmarks.ads
@@ -0,0 +1,39 @@
+-- Copyright (c) 2019 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Containers.Vectors; use Ada.Containers;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Gtk.Menu_Item; use Gtk.Menu_Item;
+with Gtkada.Builder; use Gtkada.Builder;
+
+package Bookmarks is
+
+   -- Data structure for bookmarks
+   type Bookmark_Record is record
+      -- Menu label for bookmark
+      MenuName: Unbounded_String;
+      -- Absolute path to bookmarked location
+      Path: Unbounded_String;
+   end record;
+   package Bookmarks_Container is new Vectors(Positive, Bookmark_Record);
+   -- List of all bookmarked locations
+   BookmarksList: Bookmarks_Container.Vector;
+
+   -- Go to selected bookmark location
+   procedure GoToBookmark(Self: access Gtk_Menu_Item_Record'Class);
+   -- Go to user home directory
+   procedure GoHome(Object: access Gtkada_Builder_Record'Class);
+
+end Bookmarks;
