672ca3f887ce46275547bba770f7c8fcb6bb3990
thindil@laeran.pl <thindil@laeran.pl>
2019-05-09 07:20:13 +0000

    updated starting script
    FossilOrigin-Name: 398518006f003e7a451d70e113b258363d69b28e393cd909ba188218ec85fec1

--- hunter.sh
+++ hunter.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-prefix=`dirname $0`
-cd "$prefix"
-prefix=`pwd`
+prefix=$(dirname "$0")
+cd "$prefix" || exit
+prefix=$(pwd)
 
 if [ -d lib ]; then
-   if [ -z ${XDG_DATA_DIRS+x} ]; then
-      export XDG_DATA_DIRS=/usr/share
-   fi
+   # distributed
    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$prefix/lib
    export GDK_PIXBUF_MODULE_FILE=$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
    export GDK_PIXBUF_MODULEDIR=$prefix/lib/gdk-pixbuf-2.0/2.10.0/loaders/
    export FONTCONFIG_FILE=$prefix/etc/fonts/fonts.conf
+   export XDG_DATA_DIRS=$XDG_DATA_DIRS:$prefix/share
    export GSETTINGS_BACKEND=memory
 else
-   eval `gtkada-env.sh --print-only`
+   # built-locally
+   eval "$(gtkada-env.sh --print-only)"
 fi
 
-cd bin
+cd bin || exit
 export RUNFROMSCRIPT=1
+
 ./hunter "$@"
