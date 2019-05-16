b57b53023738233a1dd3944b8cb78b3c3c179c85
thindil@laeran.pl <thindil@laeran.pl>
2019-05-16 05:26:17 +0000

    updated version number
    FossilOrigin-Name: 6f074a2d5ab72a20c0f09b0460519bf4b9e0143a34a1f91b8c12a5cb7d64e5d6

--- src/errordialog.adb
+++ src/errordialog.adb
@@ -48,7 +48,7 @@ package body ErrorDialog is
       end if;
       Append(ErrorText, Ada.Calendar.Formatting.Image(Clock));
       Append(ErrorText, LF);
-      Append(ErrorText, "0.3");
+      Append(ErrorText, "0.4");
       Append(ErrorText, LF);
       Append(ErrorText, "Exception: " & Exception_Name(An_Exception));
       Append(ErrorText, LF);
