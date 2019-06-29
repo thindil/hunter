48068153213f9f90235fa0586fcd8deff4e107ca
thindil@laeran.pl <thindil@laeran.pl>
2019-06-29 04:47:57 +0000

    updated and renamed script to manage translations
    FossilOrigin-Name: 74c1a3bf5c7bd6b34f93bf9bd939756741086f4e80df66e1e79150524bcade61

---
+++ translations.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+case $1 in
+   createpot)
+      xgettext --keyword=translatable -s -o po/hunter.pot bin/ui/hunter.glade
+      xgettext --package-name=Hunter --package-version=0.6 --msgid-bugs-address=thindil@laeran.pl --copyright-holder="Bartek thindil Jasicki" --keyword=Gettext -sj -o po/hunter.pot src/*.*
+      echo "Translation template was regenerated."
+      ;;
+   createlocale)
+      mkdir -p po/$2/LC_MESSAGES
+      msginit -i po/hunter.pot -l $2 -o po/$2/hunter.po
+      echo "Empty locale $2 was created."
+      ;;
+   update)
+      for directory in $(find po/* -maxdepth 0 -type d)
+      do
+         msgmerge -U $directory/hunter.po po/hunter.pot
+         echo "Locale ${directory:3} was updated."
+      done
+      ;;
+   generate)
+      for directory in $(find po/* -maxdepth 0 -type d)
+      do
+         msgfmt -o $directory/LC_MESSAGES/hunter.mo $directory/hunter.po
+         echo "Locale ${directory:3} was generated."
+      done
+      ;;
+   help)
+      echo "createpot                 - Regenerate translations template file"
+      echo "createlocale [localename] - Create new localization file for selected locale"
+      echo "update                    - Updated existing localization files with new strings from template file"
+      echo "generate                  - Generate localizations files for the program"
+      echo "help                      - This screen"
+      ;;
+   *)
+      echo "Unknown command, possible options are: createpot, createlocale, update, generate, help"
+      ;;
+esac
