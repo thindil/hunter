36681f9ce864f912f5a0d7c4783c016f2c1b19e4
thindil@laeran.pl <thindil@laeran.pl>
2019-07-18 07:50:08 +0000

    added missing translation
    FossilOrigin-Name: 98081d4eb0f3e9f38a05ad040e2809602777954cf651029c9f08c7169239bdf4

--- src/deleteitems.adb
+++ src/deleteitems.adb
@@ -66,7 +66,7 @@ package body DeleteItems is
 
    procedure DeleteItem(Object: access Gtkada_Builder_Record'Class) is
       pragma Unreferenced(Object);
-      Message: Unbounded_String := To_Unbounded_String("Delete?" & LF);
+      Message: Unbounded_String := To_Unbounded_String(Gettext("Delete?") & LF);
    begin
       for I in SelectedItems.First_Index .. SelectedItems.Last_Index loop
          Append(Message, SelectedItems(I));
