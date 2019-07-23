5fa3c2dbdd8947e5f9d6d0f38e69628e39f42be1
thindil@laeran.pl <thindil@laeran.pl>
2019-07-23 05:56:17 +0000

    code formatting
    FossilOrigin-Name: b55b5a91b33ed0248c272ca444c386b6652a8ba323e664e1828afad99f74a508

--- src/hunter.adb
+++ src/hunter.adb
@@ -42,10 +42,12 @@ begin
    if not Ada.Directories.Exists(Value("HOME") & "/.cache/hunter") then
       Create_Path(Value("HOME") & "/.cache/hunter");
    end if;
-   if not Ada.Directories.Exists(Value("HOME") & "/.local/share/Trash/files") then
+   if not Ada.Directories.Exists
+       (Value("HOME") & "/.local/share/Trash/files") then
       Create_Path(Value("HOME") & "/.local/share/Trash/files");
    end if;
-   if not Ada.Directories.Exists(Value("HOME") & "/.local/share/Trash/info") then
+   if not Ada.Directories.Exists
+       (Value("HOME") & "/.local/share/Trash/info") then
       Create_Path(Value("HOME") & "/.local/share/Trash/info");
    end if;
    -- Start GTK
