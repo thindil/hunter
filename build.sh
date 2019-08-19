bbae12aba4d651bdbb1bafbf47e987d5011f0374
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-08-19 08:38:26 +0000

    added build script
    FossilOrigin-Name: c3a1bbb6c9df3775478143f13738dafe6603b85be62059dc6650a1465d233a32

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+
+case $1 in
+   release)
+      gprclean
+      gprbuild -XMode=release
+      mkdir -p others/Output/release/bin
+      mkdir -p others/Output/release/share/docs
+      mkdir -p others/Output/release/share/locale
+      cp -r bin others/Output/release
+      cp CHANGELOG.md others/Output/release/share/docs/
+      cp COPYING others/Output/release/share/docs/
+      cp README.md others/Output/release/share/docs
+      for directory in $(find po/* -maxdepth 0 -type d)
+      do
+         cp -r $directory others/Output/release/share/locale/
+         rm others/Output/release/share/locale/${directory:3}/hunter.po
+      done
+      gprclean
+      ;;
+   debug)
+      grpclean
+      gprbuild
+      ;;
+   createtest)
+      gnattest -P steamsky.gpr
+      ;;
+   tests)
+      gprbuild -P tests/driver/test_driver.gpr
+      ;;
+   docs)
+      ./generatedocs
+      ;;
+   help)
+      echo "release       - Build the game in release mode"
+      echo "debug         - Build the game in debug mode"
+      echo "createtests   - Regenerate unit tests"
+      echo "tests         - Build unit tests"
+      echo "docs          - Generate code documentation"
+      echo "help          - This screen"
+      ;;
+   *)
+      echo "Unknown command, possible options are: release, debug, createtests, tests, docs, help"
+      ;;
+esac
