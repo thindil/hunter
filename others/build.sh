a77729bbf1b96114995834bcf18ad3649fcaee59
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2019-11-18 06:27:11 +0000

    updated build script
    FossilOrigin-Name: 51efc0d515adfc9b6d8741d21826d49ede5c5cc108581b9970eeeaaf8abb4b78

--- others/build.sh
+++ others/build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+releasedir=usr
+
 gprclean -P hunter.gpr
 gprbuild -P hunter.gpr -XMode=release
-mkdir -p usr/share/doc/hunter
-mkdir -p usr/share/locale
-cp -r bin usr/
-cp CHANGELOG.md usr/share/doc/hunter/
-cp COPYING usr/share/doc/hunter/
-cp README.md usr/share/doc/hunter/
-cp CONTRIBUTING.md usr/share/doc/hunter/
+mkdir -p $releasedir/share/doc/hunter
+mkdir -p $releasedir/share/locale
+mkdir -p $releasedir/share/metainfo/
+cp -r bin $releasedir/
+cp CHANGELOG.md $releasedir/share/doc/hunter/
+cp COPYING $releasedir/share/doc/hunter/
+cp README.md $releasedir/share/doc/hunter/
+cp CONTRIBUTING.md $releasedir/share/doc/hunter/
+cp others/pl.laeran.hunter.appdata.xml $releasedir/share/metainfo
 others/translations.sh generate
 for directory in $(find po/* -maxdepth 0 -type d)
 do
-   cp -r $directory usr/share/locale/
-   rm usr/share/locale/${directory:3}/hunter.po
+   cp -r $directory $releasedir/share/locale/
+   rm $releasedir/share/locale/${directory:3}/hunter.po
 done
 gprclean -P hunter.gpr
