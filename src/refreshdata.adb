8392803e4920f97848ce512e37113f7a84a8704b
thindil@laeran.pl <thindil@laeran.pl>
2019-11-21 07:10:36 +0000

    started work on updating directory listing
    FossilOrigin-Name: d40e1986ccf7feabcb888ebd1acac945a870397e115adcda6aab351419cdc584

--- src/refreshdata.adb
+++ src/refreshdata.adb
@@ -13,13 +13,26 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+with Ada.Calendar; use Ada.Calendar;
+with Ada.Calendar.Formatting; use Ada.Calendar.Formatting;
+with Ada.Calendar.Time_Zones; use Ada.Calendar.Time_Zones;
 with Ada.Containers.Indefinite_Hashed_Maps; use Ada.Containers;
+with Ada.Directories; use Ada.Directories;
 with Ada.Strings.Hash;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with GNAT.Directory_Operations; use GNAT.Directory_Operations;
+with GNAT.OS_Lib; use GNAT.OS_Lib;
 with Inotify; use Inotify;
+with Gtk.List_Store; use Gtk.List_Store;
+with Gtk.Tree_Model; use Gtk.Tree_Model;
+with Gtk.Tree_Model_Sort; use Gtk.Tree_Model_Sort;
+with Gtkada.Builder; use Gtkada.Builder;
 with Glib; use Glib;
 with Glib.Main; use Glib.Main;
+with LoadData; use LoadData;
+with MainWindow; use MainWindow;
 with Preferences; use Preferences;
-with Ada.Text_IO;
+with Utils; use Utils;
 
 package body RefreshData is
 
@@ -35,13 +48,198 @@ package body RefreshData is
       Ada.Strings.Hash, "=");
    ItemsList: Items_Container.Map;
 
+   -- ****if* RefreshData/UpdateItem
+   -- FUNCTION
+   -- Check if selected file or directory was modified and upgrade information
+   -- if was
+   -- PARAMETERS
+   -- Model - Gtk_Tree_Model with content of the current directory
+   -- Path  - Gtk_Tree_Path to selected file or directory (Unused)
+   -- Iter  - Gtk_Tree_Iter to selected file or directory
+   -- RESULT
+   -- Always return false to check each file or directory in current directory
+   -- SOURCE
+   function UpdateItem
+     (Model: Gtk_Tree_Model; Path: Gtk_Tree_Path; Iter: Gtk_Tree_Iter)
+      return Boolean is
+      -- ****
+      pragma Unreferenced(Path);
+      FileName: constant String :=
+        To_String(CurrentDirectory) & "/" & Get_String(Model, Iter, 0);
+      NewIter: Gtk_Tree_Iter := Iter;
+      Size: File_Size;
+      Directory: Dir_Type;
+      Last: Natural;
+      SubFileName: String(1 .. 1024);
+   begin
+      if ItemsList.Contains(FileName) then
+         case ItemsList(FileName) is
+            when Moved_From | Deleted =>
+               Remove(-(Model), NewIter);
+            when Metadata | Closed_Write =>
+               Set
+                 (-(Model), Iter, 5,
+                  Image
+                    (Date => Modification_Time(FileName),
+                     Time_Zone => UTC_Time_Offset));
+               if not Is_Read_Accessible_File(FileName) then
+                  Set(-(Model), Iter, 3, "?");
+                  Set(-(Model), Iter, 4, 0);
+                  ItemsList.Delete(FileName);
+                  return False;
+               end if;
+               if Is_Directory(FileName) then
+                  Open(Directory, FileName);
+                  Size := 0;
+                  loop
+                     Read(Directory, SubFileName, Last);
+                     exit when Last = 0;
+                     if SubFileName(1 .. Last) /= "." and
+                       SubFileName(1 .. Last) /= ".." then
+                        Size := Size + 1;
+                     end if;
+                  end loop;
+                  Close(Directory);
+                  Set(-(Model), Iter, 3, File_Size'Image(Size));
+               elsif Is_Regular_File(FileName) then
+                  Size := Ada.Directories.Size(FileName);
+                  Set(-(Model), Iter, 3, CountFileSize(Size));
+                  if Size > File_Size(Gint'Last) then
+                     Size := File_Size(Gint'Last);
+                  end if;
+                  Set(-(Model), Iter, 4, Gint(Size));
+               end if;
+            when others =>
+               null;
+         end case;
+         ItemsList.Delete(FileName);
+      end if;
+      return False;
+   end UpdateItem;
+
    function CheckItems return Boolean is
+      FilesList: constant Gtk_List_Store :=
+        Gtk_List_Store(Get_Object(Builder, "fileslist"));
+      FileIter: Gtk_Tree_Iter := Get_Iter_First(FilesList);
+      Size: File_Size;
+      SubDirectory: Dir_Type;
+      SubLast: Natural;
+      SubFileName: String(1 .. 1024);
+      MimeType: Unbounded_String;
    begin
       if not TemporaryStop then
+         Foreach
+           (Gtk_List_Store(Get_Object(Builder, "fileslist")),
+            UpdateItem'Access);
+         if ItemsList.Length = 0 then
+            return True;
+         end if;
+         Set_Sort_Func
+           (Gtk_Tree_Model_Sort(Get_Object(Builder, "filessort")), 0,
+            EmptySortFiles'Access);
          for I in ItemsList.Iterate loop
-            Ada.Text_IO.Put_Line
-              (Items_Container.Key(I) & " " & Event_Kind'Image(ItemsList(I)));
+            if ItemsList(I) /= Created and ItemsList(I) /= Moved_To then
+               goto End_Of_Loop;
+            end if;
+            Append(FilesList, FileIter);
+            begin
+               Set
+                 (FilesList, FileIter, 5,
+                  Ada.Calendar.Formatting.Image
+                    (Date => Modification_Time(Items_Container.Key(I)),
+                     Time_Zone => UTC_Time_Offset));
+            exception
+               when others =>
+                  Set(FilesList, FileIter, 5, "unknown");
+            end;
+            Set(FilesList, FileIter, 6, Items_Container.Key(I));
+            if Is_Directory(Items_Container.Key(I)) then
+               if Simple_Name(Items_Container.Key(I))(1) = '.' then
+                  Set(FilesList, FileIter, 1, 1);
+               else
+                  Set(FilesList, FileIter, 1, 2);
+               end if;
+               if Is_Symbolic_Link(Items_Container.Key(I)) then
+                  Set(FilesList, FileIter, 2, "emblem-symbolic-link");
+               else
+                  Set(FilesList, FileIter, 2, "folder");
+               end if;
+               Set(FilesList, FileIter, 4, Gint'Last);
+               if Is_Read_Accessible_File(Items_Container.Key(I)) then
+                  Open(SubDirectory, Items_Container.Key(I));
+                  Size := 0;
+                  loop
+                     Read(SubDirectory, SubFileName, SubLast);
+                     exit when SubLast = 0;
+                     if SubFileName(1 .. SubLast) /= "." and
+                       SubFileName(1 .. SubLast) /= ".." then
+                        Size := Size + 1;
+                     end if;
+                  end loop;
+                  Close(SubDirectory);
+                  Set(FilesList, FileIter, 3, File_Size'Image(Size));
+               else
+                  Set(FilesList, FileIter, 3, "?");
+               end if;
+            else
+               if Simple_Name(Items_Container.Key(I))(1) = '.' then
+                  Set(FilesList, FileIter, 1, 3);
+               else
+                  Set(FilesList, FileIter, 1, 4);
+               end if;
+               if Is_Symbolic_Link(Items_Container.Key(I)) then
+                  Set(FilesList, FileIter, 2, "emblem-symbolic-link");
+               elsif Is_Executable_File(Items_Container.Key(I)) then
+                  Set(FilesList, FileIter, 2, "application-x-executable");
+               else
+                  MimeType :=
+                    To_Unbounded_String(GetMimeType(Items_Container.Key(I)));
+                  if Index(MimeType, "audio") > 0 then
+                     Set(FilesList, FileIter, 2, "audio-x-generic");
+                  elsif Index(MimeType, "font") > 0 then
+                     Set(FilesList, FileIter, 2, "font-x-generic");
+                  elsif Index(MimeType, "image") > 0 then
+                     Set(FilesList, FileIter, 2, "image-x-generic");
+                  elsif Index(MimeType, "video") > 0 then
+                     Set(FilesList, FileIter, 2, "video-x-generic");
+                  elsif Index(MimeType, "text/x-script") > 0 then
+                     Set(FilesList, FileIter, 2, "text-x-script");
+                  elsif MimeType = To_Unbounded_String("text/html") then
+                     Set(FilesList, FileIter, 2, "text-html");
+                  elsif Index(MimeType, "zip") > 0 or
+                    Index(MimeType, "x-xz") > 0 then
+                     Set(FilesList, FileIter, 2, "package-x-generic");
+                  elsif Index(MimeType, "text") > 0 then
+                     Set(FilesList, FileIter, 2, "text-x-generic");
+                  else
+                     Set(FilesList, FileIter, 2, "text-x-generic-template");
+                  end if;
+               end if;
+               if not Is_Read_Accessible_File(Items_Container.Key(I)) then
+                  Set(FilesList, FileIter, 3, "?");
+                  Set(FilesList, FileIter, 4, 0);
+                  goto End_Of_Loop;
+               end if;
+               if Is_Symbolic_Link(Items_Container.Key(I)) then
+                  Set(FilesList, FileIter, 3, "->");
+                  Set(FilesList, FileIter, 4, 0);
+               elsif Is_Regular_File(Items_Container.Key(I)) then
+                  Size := Ada.Directories.Size(Items_Container.Key(I));
+                  Set(FilesList, FileIter, 3, CountFileSize(Size));
+                  if Size > File_Size(Gint'Last) then
+                     Size := File_Size(Gint'Last);
+                  end if;
+                  Set(FilesList, FileIter, 4, Gint(Size));
+               else
+                  Set(FilesList, FileIter, 3, "0");
+                  Set(FilesList, FileIter, 4, 0);
+               end if;
+            end if;
+            <<End_Of_Loop>>
          end loop;
+         Set_Sort_Func
+           (Gtk_Tree_Model_Sort(Get_Object(Builder, "filessort")), 0,
+            SortFiles'Access);
       end if;
       ItemsList.Clear;
       return True;
