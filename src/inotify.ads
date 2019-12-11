e9981c58780ab3fdaf5a08a81bb4fb1eaf3beb96
thindil@laeran.pl <thindil@laeran.pl>
2019-12-11 06:32:46 +0000

    added code documentation
    FossilOrigin-Name: 423badd0740bf50585854c11f6a440e4eb3c904ac126ffdac85c8f72d44471de

--- src/inotify.ads
+++ src/inotify.ads
@@ -16,8 +16,17 @@
 with Ada.Containers.Vectors; use Ada.Containers;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
+-- ****h* Hunter/Inotify
+-- FUNCTION
+-- Small Ada binding to kernel inotify
+-- SOURCE
 package Inotify is
+-- ****
 
+   -- ****t* Inotify/Inotify_Events
+   -- FUNCTION
+   -- Types of inotify events and values for them
+   -- SOURCE
    type Inotify_Events is
      (Accessed, Modified, Metadata, Closed_Write, Closed_No_Write, Opened,
       Moved_From, Moved_To, Created, Deleted, Deleted_Self, Moved_Self,
@@ -29,21 +38,74 @@ package Inotify is
       Moved_To => 16#0080#, Created => 16#0100#, Deleted => 16#0200#,
       Deleted_Self => 16#0400#, Moved_Self => 16#0800#,
       Unmounted_Filesystem => 16#2000#);
+   -- ****
 
+   -- ****t* Inotify/Event_Data
+   -- FUNCTION
+   -- Data structure used to store information about inotify event
+   -- OPTIONS
+   -- Event  - inotify event
+   -- Target - target file or directory
+   -- Path   - path in which event occurs
+   -- SOURCE
    type Event_Data is record
       Event: Inotify_Events;
       Target: Unbounded_String;
       Path: Unbounded_String;
    end record;
+   -- ****
 
+   -- ****t* Inotify/Events_Container
+   -- FUNCTION
+   -- Used to store data about inotify events
+   -- SOURCE
    package Events_Container is new Vectors(Positive, Event_Data);
+   -- ****
 
+   -- ****v* Inotify/EventsList
+   -- FUNCTION
+   -- List of all caught inotify events
+   -- SOURCE
    EventsList: Events_Container.Vector;
+   -- ****
 
+   -- ****f* Inotify/InotifyInit
+   -- FUNCTION
+   -- Start inotify instance
+   -- SOURCE
    procedure InotifyInit;
+   -- ****
+
+   -- ****f* Inotify/InotifyClose
+   -- FUNCTION
+   -- Close inotify instance
+   -- SOURCE
    procedure InotifyClose;
+   -- ****
+
+   -- ****f* Inotify/AddWatches
+   -- FUNCTION
+   -- Add inotify watches for selected path and all subdirectories inside it,
+   -- but not deeper.
+   -- PARAMETERS
+   -- Path - Full path to which inotify watches will be added
+   -- SOURCE
    procedure AddWatches(Path: String);
+   -- ****
+
+   -- ****f* Inotify/RemoveWatches
+   -- FUNCTION
+   -- Remove all inotify watches
+   -- SOURCE
    procedure RemoveWatches;
+   -- ****
+
+   -- ****f* Inotify/InotifyRead
+   -- FUNCTION
+   -- Read all caught and waiting inotify events and store them in EventsList.
+   -- If need, remove and add new watches also.
+   -- SOURCE
    procedure InotifyRead;
+   -- ****
 
 end Inotify;
