97f6262b12d0e4f391df49e7b00cae564f7d91dd
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-01-26 06:16:50 +0000

    updated build script
    FossilOrigin-Name: f1b50d64b0c4421f542a1f7c8042c9d68c3f8f1aa352c5055b26725d633aa1ae

--- others/build.sh
+++ others/build.sh
@@ -5,7 +5,6 @@ releasedir=usr
 gprclean -P hunter.gpr
 gprbuild -P hunter.gpr -XMode=release
 mkdir -p $releasedir/share/doc/hunter
-mkdir -p $releasedir/share/locale
 mkdir -p $releasedir/share/metainfo/
 cp -r bin $releasedir/
 cp CHANGELOG.md $releasedir/share/doc/hunter/
@@ -13,10 +12,4 @@ cp COPYING $releasedir/share/doc/hunter/
 cp README.md $releasedir/share/doc/hunter/
 cp CONTRIBUTING.md $releasedir/share/doc/hunter/
 cp others/hunter.appdata.xml $releasedir/share/metainfo
-others/translations.sh generate
-for directory in $(find po/* -maxdepth 0 -type d)
-do
-   cp -r $directory $releasedir/share/locale/
-   rm $releasedir/share/locale/${directory:3}/hunter.po
-done
 gprclean -P hunter.gpr
