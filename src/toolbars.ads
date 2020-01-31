1953c58b7ac0ac66d54181b6c4a919c62630952c
thindil@laeran.pl <thindil@laeran.pl>
2020-01-31 06:23:19 +0000

    moved setting toolbars outside creating code
    FossilOrigin-Name: 5c97e8364c451e0ee8811facc4e47931bf0c9cca533c80959c3817a4944d4d60

--- src/toolbars.ads
+++ src/toolbars.ads
@@ -13,7 +13,19 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+-- ****h* Hunter/Toolbars
+-- FUNCTION
+-- Provide code for manipulate the program toolbars
+-- SOURCE
 package Toolbars is
+-- ****
+
+   -- ****f* Toolbars/SetToolbars
+   -- FUNCTION
+   -- Set proper orientation for the toolbars;
+   -- SOURCE
+   procedure SetToolbars;
+   -- ****
 
    -- ****f* Toolbars/CreateActionToolbar
    -- FUNCTION
