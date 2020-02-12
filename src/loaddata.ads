ccd7c5177c19f2fccaf5f1f433645e572ae98a8e
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-02-12 06:51:58 +0000

    started work on showing directory content
    FossilOrigin-Name: 4698879b748decc88b82ab855f18d6570ae0b79f88cb02e10311fd164b0417df

--- src/loaddata.ads
+++ src/loaddata.ads
@@ -13,9 +13,12 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-with Gtk.List_Store; use Gtk.List_Store;
-with Gtk.Tree_Model; use Gtk.Tree_Model;
-with Glib; use Glib;
+--with Gtk.List_Store; use Gtk.List_Store;
+--with Gtk.Tree_Model; use Gtk.Tree_Model;
+--with Glib; use Glib;
+with Ada.Calendar; use Ada.Calendar;
+with Ada.Containers.Vectors; use Ada.Containers;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 -- ****h* Hunter/LoadData
 -- FUNCTION
@@ -37,8 +40,8 @@ package LoadData is
    -- 0 if first element should be sort with second (equal)
    -- -1 if first element should be sort before second
    -- SOURCE
-   function SortFiles
-     (Model: Gtk_Tree_Model; A: Gtk_Tree_Iter; B: Gtk_Tree_Iter) return Gint;
+--   function SortFiles
+--     (Model: Gtk_Tree_Model; A: Gtk_Tree_Iter; B: Gtk_Tree_Iter) return Gint;
    -- ****
 
    -- ****if* LoadData/EmptySortFiles
@@ -53,8 +56,8 @@ package LoadData is
    -- RESULT
    -- This function always return 0;
    -- SOURCE
-   function EmptySortFiles
-     (Model: Gtk_Tree_Model; A: Gtk_Tree_Iter; B: Gtk_Tree_Iter) return Gint;
+--   function EmptySortFiles
+--     (Model: Gtk_Tree_Model; A: Gtk_Tree_Iter; B: Gtk_Tree_Iter) return Gint;
    -- ****
 
    -- ****f* LoadData/LoadDirectory
@@ -66,7 +69,7 @@ package LoadData is
    --            are: "fileslist" for current directory and "fileslist1" for
    --            directory preview
    -- SOURCE
-   procedure LoadDirectory(Name, ListName: String);
+--   procedure LoadDirectory(Name, ListName: String);
    -- ****
 
    -- ****f* LoadData/AddItem
@@ -81,8 +84,25 @@ package LoadData is
    -- RESULT
    -- Parameter FileIter
    -- SOURCE
-   procedure AddItem
-     (FilesList: Gtk_List_Store; FileIter: out Gtk_Tree_Iter; Path: String);
+--   procedure AddItem
+--     (FilesList: Gtk_List_Store; FileIter: out Gtk_Tree_Iter; Path: String);
    -- ****
+
+   type ItemType is (File, Directory);
+
+   type Item_Record is record
+      Name: Unbounded_String;
+      Size: Unbounded_String;
+      IType: ItemType;
+      Modified: Time;
+      Hidden: Boolean;
+      Image: Unbounded_String;
+   end record;
+
+   package Items_Container is new Vectors(Positive, Item_Record);
+
+   ItemsList: Items_Container.Vector;
+
+   procedure LoadDirectory(DirectoryName: String);
 
 end LoadData;
