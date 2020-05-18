98a29130244ea26467c9565d8f7badf2276e0a37
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-05-18 05:23:19 +0000

    continue work on the program configuration UI
    FossilOrigin-Name: 2f960ec0a6a6eb69d40b0d2f463c835f4ca147282690ba94e005cbf86b6ab939

--- src/preferences-commands.adb
+++ src/preferences-commands.adb
@@ -22,13 +22,15 @@ with Tcl.Tk.Ada; use Tcl.Tk.Ada;
 with Tcl.Tk.Ada.Busy; use Tcl.Tk.Ada.Busy;
 with Tcl.Tk.Ada.Pack;
 with Tcl.Tk.Ada.Widgets; use Tcl.Tk.Ada.Widgets;
-with Tcl.Tk.Ada.Widgets.TtkButton; use Tcl.Tk.Ada.Widgets.TtkButton;
-with Tcl.Tk.Ada.Widgets.TtkButton.TtkCheckButton;
-use Tcl.Tk.Ada.Widgets.TtkButton.TtkCheckButton;
-with Tcl.Tk.Ada.Widgets.TtkLabelFrame; use Tcl.Tk.Ada.Widgets.TtkLabelFrame;
 with Tcl.Tk.Ada.Widgets.Toplevel; use Tcl.Tk.Ada.Widgets.Toplevel;
 with Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
 use Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
+with Tcl.Tk.Ada.Widgets.TtkButton; use Tcl.Tk.Ada.Widgets.TtkButton;
+with Tcl.Tk.Ada.Widgets.TtkButton.TtkCheckButton;
+use Tcl.Tk.Ada.Widgets.TtkButton.TtkCheckButton;
+with Tcl.Tk.Ada.Widgets.TtkLabel; use Tcl.Tk.Ada.Widgets.TtkLabel;
+with Tcl.Tk.Ada.Widgets.TtkLabelFrame; use Tcl.Tk.Ada.Widgets.TtkLabelFrame;
+with Tcl.Tk.Ada.Widgets.TtkScale; use Tcl.Tk.Ada.Widgets.TtkScale;
 with Tcl.Tk.Ada.Winfo; use Tcl.Tk.Ada.Winfo;
 with Tcl.Tklib.Ada.Tooltip; use Tcl.Tklib.Ada.Tooltip;
 with Utils; use Utils;
@@ -42,13 +44,50 @@ package body Preferences.Commands is
    package CreateCommands is new Tcl.Ada.Generic_Command(Integer);
    -- ****
 
+   function Set_Label_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int with
+      Convention => C;
+
+      -- ****if* Commands/Set_Label_Command
+      -- FUNCTION
+      -- Update text of the selected label
+      -- PARAMETERS
+      -- ClientData - Custom data send to the command. Unused
+      -- Interp     - Tcl interpreter in which command was executed.
+      -- Argc       - Number of arguments passed to the command. Unused
+      -- Argv       - Values of arguments passed to the command.
+      -- SOURCE
+   function Set_Label_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int is
+      pragma Unreferenced(ClientData, Argc);
+      -- ****
+      Label: Ttk_Label;
+   begin
+      Label.Interp := Interp;
+      Label.Name :=
+        New_String(".preferencesdialog." & CArgv.Arg(Argv, 1) & "label");
+      if CArgv.Arg(Argv, 1) = "directory.interval" then
+         Tcl.Tk.Ada.Widgets.configure
+           (Label,
+            "-text {Auto refresh every " &
+            Natural'Image
+              (Natural(Float'Value(Tcl_GetVar(Interp, "updateinterval")))) &
+            " seconds}");
+      end if;
+      return TCL_OK;
+   end Set_Label_Command;
+
    function Show_Preferences_Command
      (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
       Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
       return Interfaces.C.int with
       Convention => C;
 
-      -- ****if* Commands/GoToBookmark_Command
+      -- ****if* Commands/Show_Preferences_Command
       -- FUNCTION
       -- Go to the selected bookmarked directory
       -- PARAMETERS
@@ -73,6 +112,8 @@ package body Preferences.Commands is
            "-text {Close} -command {CloseDialog " &
            Widget_Image(PreferencesDialog) & "} -underline 0");
       CheckButton: Ttk_CheckButton;
+      Label: Ttk_Label;
+      Scale: Ttk_Scale;
    begin
       if Tcl.Tk.Ada.Busy.Status(MainWindow) = "0" then
          Tcl.Tk.Ada.Busy.Busy(MainWindow);
@@ -92,7 +133,7 @@ package body Preferences.Commands is
       end if;
       Add
         (CheckButton,
-         "Show hidden files and directories in directory listing and in directories preview.");
+         "Show hidden files and directories in directory\nlisting and in directories preview.");
       Tcl.Tk.Ada.Pack.Pack(CheckButton, "-fill x");
       CheckButton :=
         Create
@@ -105,7 +146,38 @@ package body Preferences.Commands is
       end if;
       Add
         (CheckButton,
-         "Show the column with last modification date for files and directories.");
+         "Show the column with last modification\ndate for files and directories.");
+      Tcl.Tk.Ada.Pack.Pack(CheckButton, "-fill x");
+      Tcl_SetVar
+        (CheckButton.Interp, "updateinterval",
+         Natural'Image(Settings.AutoRefreshInterval));
+      Label :=
+        Create
+          (Widget_Image(LabelFrame) & ".intervallabel",
+           "-text ""Auto refresh every $updateinterval seconds""");
+      Tcl.Tk.Ada.Pack.Pack(Label, "-fill x");
+      Scale :=
+        Create
+          (Widget_Image(LabelFrame) & ".intervalscale",
+           "-from 0 -to 30 -variable updateinterval -orient horizontal -command {SetLabel directory.interval}");
+      Add
+        (Scale,
+         "How often (in seconds) the program should check\nfor changes in current directory.\nIf set to zero, autorefresh will be disabled.");
+      Tcl.Tk.Ada.Pack.Pack(Scale, "-fill x");
+      Tcl.Tk.Ada.Pack.Pack(LabelFrame, "-fill x");
+      LabelFrame :=
+        Create(Widget_Image(PreferencesDialog) & ".preview", "-text Preview");
+      CheckButton :=
+        Create
+          (Widget_Image(LabelFrame) & ".showpreview", "-text {Show preview}");
+      if Settings.ShowPreview then
+         Tcl_SetVar(CheckButton.Interp, Widget_Image(CheckButton), "1");
+      else
+         Tcl_SetVar(CheckButton.Interp, Widget_Image(CheckButton), "0");
+      end if;
+      Add
+        (CheckButton,
+         "Show second panel with preview of files and directories.\nIf you disable this option, second panel will be visible only during\ncopying and moving files or directories and during creating new link.");
       Tcl.Tk.Ada.Pack.Pack(CheckButton, "-fill x");
       Tcl.Tk.Ada.Pack.Pack(LabelFrame, "-fill x");
       Tcl.Tk.Ada.Pack.Pack(CloseButton);
@@ -155,6 +227,7 @@ package body Preferences.Commands is
    begin
       AddCommand("ShowPreferences", Show_Preferences_Command'Access);
       AddCommand("CloseDialog", Close_Dialog_Command'Access);
+      AddCommand("SetLabel", Set_Label_Command'Access);
    end AddCommands;
 
 end Preferences.Commands;
