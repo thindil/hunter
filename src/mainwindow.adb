8d8fd26cb66b00a3f8d683248ac1b4c6a041fadf
thindil@laeran.pl <thindil@laeran.pl>
2020-05-28 08:12:22 +0000

    redesigned header label
    FossilOrigin-Name: 57e4ca1ec9241a136053f49081645c0f7446393a6ca47e7263d1bb62ca5ab46b

--- src/mainwindow.adb
+++ src/mainwindow.adb
@@ -106,7 +106,7 @@ package body MainWindow is
       Button: Ttk_Button;
       PathButtonsFrame: constant Ttk_Frame :=
         Create(".mainframe.paned.directoryframe.pathframe");
-      pragma Unreferenced(Image, ProgressBar);
+      pragma Unreferenced(Image, ProgressBar, HeaderLabel);
    begin
       AddCommands;
       CreateSearchUI;
@@ -178,7 +178,6 @@ package body MainWindow is
       Bind(DirectoryTree, "<<TreeviewSelect>>", "ShowSelected");
       Tcl.Tk.Ada.Pack.Pack(DirectoryTree, "-side top -fill both -expand true");
       if not Settings.ToolbarsOnTop then
-         Tcl.Tk.Ada.Grid.Grid(HeaderLabel, "-column 0 -row 0 -columnspan 2");
          Tcl.Tk.Ada.Grid.Grid(Paned, "-column 1 -row 3 -sticky nswe");
       else
          Tcl.Tk.Ada.Grid.Grid(Paned, "-column 0 -row 3 -sticky nswe");
