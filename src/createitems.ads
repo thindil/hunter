1d8d4cfc0e0fa7ef68e70d16aba0af63b638f5b6
thindil@laeran.pl <thindil@laeran.pl>
2020-07-02 08:04:24 +0000

    removed unused code
    FossilOrigin-Name: 0c9125d8455e96082ada932e302a8f67172e91d88fd1416eeca59d3208b3f295

--- src/createitems.ads
+++ src/createitems.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2019 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2019-2020 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -13,21 +13,12 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
---with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-
 -- ****h* Hunter/CreateItems
 -- FUNCTION
 -- Provide code for create files and directories.
 -- SOURCE
 package CreateItems is
 -- ****
-
-   -- ****v* CreateItems/LinkTarget
-   -- FUNCTION
-   -- Destination for new symbolic link
-   -- SOURCE
---   LinkTarget: Unbounded_String;
-   -- ****
 
    -- ****f* CreateItems/CreateCreateUI
    -- FUNCTION
