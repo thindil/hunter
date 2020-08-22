aff2ca8cb26f8727dbef543003ba0ee802b1b480
thindil@laeran.pl <thindil@laeran.pl>
2020-08-22 08:44:16 +0000

    updated code documentation
    FossilOrigin-Name: e77b651870dc494578a89c4a4436c78f7072edb62648f90f1d814ad23f6305ee

--- src/createitems.ads
+++ src/createitems.ads
@@ -13,7 +13,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- ****h* Hunter/CreateItems
+-- ****h* CreateItems/CreateItems
 -- FUNCTION
 -- Provide code for create files and directories.
 -- SOURCE
