03f04fbc419b05466f4d065fec9f1fdf2858eb4a
thindil@laeran.pl <thindil@laeran.pl>
2020-08-25 05:37:37 +0000

    updated code documentation
    FossilOrigin-Name: 2a9b95e83862979d5742dbb421fb5dad84e46d7a8ef6c697ac3a8ba7000585f4

--- src/messages.ads
+++ src/messages.ads
@@ -13,7 +13,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- ****h* Hunter/Messages
+-- ****h* Messages/Messages
 -- FUNCTION
 -- Provide code to show or hide messages to the user.
 -- SOURCE
