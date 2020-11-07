41cfa34bbda6e93901b4175c5376773a4d47815a
thindil@laeran.pl <thindil@laeran.pl>
2020-11-07 07:06:55 +0000

    program modules: added triggers on_start and on_quit
    FossilOrigin-Name: 6945890958afc55e6af16e37ea3fc3f4c7f1949245107adb441b4992800b2db5

---
+++ src/modules.adb
@@ -0,0 +1,57 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Directories; use Ada.Directories;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with GNAT.OS_Lib; use GNAT.OS_Lib;
+with Tcl.Ada; use Tcl.Ada;
+with Tcl.Tk.Ada; use Tcl.Tk.Ada;
+
+package body Modules is
+
+   procedure LoadModules is
+      FullPath: Unbounded_String;
+   begin
+      for ModulePath of Enabled_Modules loop
+         FullPath :=
+           To_Unbounded_String
+             (Normalize_Pathname
+                (To_String(ModulePath), Containing_Directory(Command_Name)));
+         begin
+            Tcl_EvalFile(Get_Context, To_String(FullPath) & "/module.tcl");
+            Tcl_Eval
+              (Get_Context, Simple_Name(To_String(ModulePath)) & "::on_start");
+         exception
+            when Tcl_Error_Exception =>
+               null;
+         end;
+      end loop;
+   end LoadModules;
+
+   procedure UnloadModules is
+   begin
+      for ModulePath of Enabled_Modules loop
+         begin
+            Tcl_Eval
+              (Get_Context, Simple_Name(To_String(ModulePath)) & "::on_quit");
+         exception
+            when Tcl_Error_Exception =>
+               null;
+         end;
+      end loop;
+   end UnloadModules;
+
+end Modules;
