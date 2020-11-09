651d2896a6ad036d76d10932aaa273da0f0fe668
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-11-09 06:29:48 +0000

    added Tcl command GetConfig
    FossilOrigin-Name: 1b7c5dbc5ab7202c6bae9acc5cbc260a13395c04ceb30152aa59c5cd145d1e65

--- src/modules-commands.adb
+++ src/modules-commands.adb
@@ -15,12 +15,16 @@
 
 with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Directories; use Ada.Directories;
+with Ada.Strings; use Ada.Strings;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Interfaces.C;
+with Interfaces.C; use Interfaces.C;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
 with GNAT.OS_Lib; use GNAT.OS_Lib;
 with CArgv; use CArgv;
 with Tcl; use Tcl;
 with Tcl.Ada; use Tcl.Ada;
+with Preferences; use Preferences;
 with Utils; use Utils;
 
 package body Modules.Commands is
@@ -59,7 +63,8 @@ package body Modules.Commands is
       if Enabled_Modules.Contains(ModulePath) then
          Enabled_Modules.Delete(Enabled_Modules.Find_Index(ModulePath));
          begin
-            Tcl_Eval(Interp, Simple_Name(To_String(ModulePath)) & "::on_disable");
+            Tcl_Eval
+              (Interp, Simple_Name(To_String(ModulePath)) & "::on_disable");
          exception
             when Tcl_Error_Exception =>
                null;
@@ -70,7 +75,8 @@ package body Modules.Commands is
          Enabled_Modules.Append(ModulePath);
          Tcl_EvalFile(Interp, To_String(ModulePath) & "/module.tcl");
          begin
-            Tcl_Eval(Interp, Simple_Name(To_String(ModulePath)) & "::on_enable");
+            Tcl_Eval
+              (Interp, Simple_Name(To_String(ModulePath)) & "::on_enable");
          exception
             when Tcl_Error_Exception =>
                null;
@@ -82,9 +88,104 @@ package body Modules.Commands is
          return TCL_ERROR;
    end Toggle_Module_Command;
 
+   -- ****o* MCommands2/MCommands2.Get_Config_Command
+   -- FUNCTION
+   -- Get the value of the selected configuration option of the program
+   -- PARAMETERS
+   -- ClientData - Custom data send to the command. Unused
+   -- Interp     - Tcl interpreter in which command was executed.
+   -- Argc       - Number of arguments passed to the command.
+   -- Argv       - Values of arguments passed to the command.
+   -- RESULT
+   -- This function always return TCL_OK
+   -- COMMANDS
+   -- GetConfig optionname
+   -- Optionname is the name of the program's option which value will be get
+   -- SOURCE
+   function Get_Config_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int with
+      Convention => C;
+      -- ****
+
+   function Get_Config_Command
+     (ClientData: in Integer; Interp: in Tcl.Tcl_Interp;
+      Argc: in Interfaces.C.int; Argv: in CArgv.Chars_Ptr_Ptr)
+      return Interfaces.C.int is
+      pragma Unreferenced(ClientData);
+      procedure GetBoolean(Value: Boolean) is
+      begin
+         if Value then
+            Tcl_SetResult(Interp, "1");
+         else
+            Tcl_SetResult(Interp, "0");
+         end if;
+      end GetBoolean;
+   begin
+      if Argc /= 2 then
+         Tcl_AddErrorInfo
+           (Interp,
+            New_String("Command GetConfig must have exactly one parameter"));
+         return TCL_ERROR;
+      end if;
+      if CArgv.Arg(Argv, 1) = "showhidden" then
+         GetBoolean(Settings.ShowHidden);
+      elsif CArgv.Arg(Argv, 1) = "showlastmodified" then
+         GetBoolean(Settings.ShowLastModified);
+      elsif CArgv.Arg(Argv, 1) = "scaleimages" then
+         GetBoolean(Settings.ScaleImages);
+      elsif CArgv.Arg(Argv, 1) = "autoclosemessagestime" then
+         Tcl_SetResult
+           (Interp, Trim(Natural'Image(Settings.AutoCloseMessagesTime), Left));
+      elsif CArgv.Arg(Argv, 1) = "windowwidth" then
+         Tcl_SetResult
+           (Interp, Trim(Positive'Image(Settings.WindowWidth), Left));
+      elsif CArgv.Arg(Argv, 1) = "windowheight" then
+         Tcl_SetResult
+           (Interp, Trim(Positive'Image(Settings.WindowHeight), Left));
+      elsif CArgv.Arg(Argv, 1) = "showpreview" then
+         GetBoolean(Settings.ShowPreview);
+      elsif CArgv.Arg(Argv, 1) = "stayinold" then
+         GetBoolean(Settings.StayInOld);
+      elsif CArgv.Arg(Argv, 1) = "colortext" then
+         GetBoolean(Settings.ColorText);
+      elsif CArgv.Arg(Argv, 1) = "colortheme" then
+         Tcl_SetResult(Interp, To_String(Settings.ColorTheme));
+      elsif CArgv.Arg(Argv, 1) = "deletefiles" then
+         GetBoolean(Settings.DeleteFiles);
+      elsif CArgv.Arg(Argv, 1) = "cleartrashonexit" then
+         GetBoolean(Settings.ClearTrashOnExit);
+      elsif CArgv.Arg(Argv, 1) = "showfinishedinfo" then
+         GetBoolean(Settings.ShowFinishedInfo);
+      elsif CArgv.Arg(Argv, 1) = "overwriteonexist" then
+         GetBoolean(Settings.OverwriteOnExist);
+      elsif CArgv.Arg(Argv, 1) = "toolbarsontop" then
+         GetBoolean(Settings.ToolbarsOnTop);
+      elsif CArgv.Arg(Argv, 1) = "autorefreshinterval" then
+         Tcl_SetResult
+           (Interp, Trim(Natural'Image(Settings.AutoRefreshInterval), Left));
+      elsif CArgv.Arg(Argv, 1) = "uitheme" then
+         Tcl_SetResult(Interp, To_String(Settings.UITheme));
+      elsif CArgv.Arg(Argv, 1) = "toolbarssize" then
+         Tcl_SetResult
+           (Interp, Trim(Positive'Image(Settings.ToolbarsSize), Left));
+      elsif CArgv.Arg(Argv, 1) = "monospacefont" then
+         GetBoolean(Settings.MonospaceFont);
+      else
+         Tcl_AddErrorInfo
+           (Interp,
+            New_String
+              ("Parameter for GetConfig must be one of: showhidden, showlastmodified, scaleimages, autoclosemessagestime, windowwidth, windowheight, showpreview, stayinold, colortext, colortheme, deletefiles, cleartrashonexit, showfinishedinfo, overwriteonexits, toolsbarsontop, autorefreshinterval, uitheme, toolbarssize, monospacefont"));
+         return TCL_ERROR;
+      end if;
+      return TCL_OK;
+   end Get_Config_Command;
+
    procedure AddCommands is
    begin
       AddCommand("ToggleModule", Toggle_Module_Command'Access);
+      AddCommand("GetConfig", Get_Config_Command'Access);
    end AddCommands;
 
 end Modules.Commands;
