99d7c5b89c97b65b8644f2951b609cde32571b02
thindil@laeran.pl <thindil@laeran.pl>
2020-11-12 08:38:02 +0000

    continue work on bob module
    FossilOrigin-Name: 4909269c7ec8577d25d2a9b1fa364e0143d782b7adb5975754427f84c043d589

--- share/hunter/modules/bob/module.tcl
+++ share/hunter/modules/bob/module.tcl
@@ -1,21 +1,21 @@
 namespace eval bob {
 
-   proc Create_UI {} {
-      image create photo BobIcon -file bob.svg -format "svg -scaletoheight [GetConfig toolbarssize]"
+   proc Create_UI {path} {
+      image create photo BobIcon -file [file join $path bob.svg] -format "svg -scaletoheight [GetConfig toolbarssize]"
       ttk::menubutton .actiontoolbar.bobbutton -style Toolbutton -image BobIcon -takefocus 0
       pack .actiontoolbar.bobbutton -after .actiontoolbar.deletebutton
    }
 
-   proc on_enable {} {
-      bob::Create_UI
+   proc on_enable {path} {
+      bob::Create_UI $path
    }
 
    proc on_disable {} {
       puts {I'm disabled :(}
    }
 
-   proc on_start {} {
-      bob::Create_UI
+   proc on_start {path} {
+      bob::Create_UI $path
    }
 
    proc on_enter {path} {
