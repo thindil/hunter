cb32447c192b4907af030a8ff210d90d02f1f844
thindil@laeran.pl <thindil@laeran.pl>
2020-11-13 08:37:35 +0000

    small code cleanup
    FossilOrigin-Name: 5fc92c4a3bed04d35d6443dff92b004ccd2e89098e0f3aae4ce4d50e98360691

--- share/hunter/modules/bob/module.tcl
+++ share/hunter/modules/bob/module.tcl
@@ -30,10 +30,9 @@ namespace eval bob {
       .bobmenu delete 0 end
       set bobfile [open $filename]
       while {[gets $bobfile line] >= 0} {
-         if {[string first {name:} $line] == -1} {
-            continue
+         if {[string first {name:} $line] == 3} {
+            .bobmenu add command -label [string range $line 9 end]
          }
-         .bobmenu add command -label [string range $line 9 end]
       }
       close $bobfile
    }
