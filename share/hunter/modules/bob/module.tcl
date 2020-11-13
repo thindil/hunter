cfcc83f31cf18999eadeb7521e4e6d8398fa940c
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-11-13 07:10:58 +0000

    continue work on bob module
    FossilOrigin-Name: 02cc9418a304aad41ba86cf0bf253de3e1f5687d08adf53f70744a9eeee60ce3

--- share/hunter/modules/bob/module.tcl
+++ share/hunter/modules/bob/module.tcl
@@ -2,8 +2,8 @@ namespace eval bob {
 
    proc Create_UI {path} {
       image create photo BobIcon -file [file join $path bob.svg] -format "svg -scaletoheight [GetConfig toolbarssize]"
-      ttk::menubutton .mainframe.toolbars.actiontoolbar.bobbutton -style Toolbutton -image BobIcon -takefocus 0
-      pack .mainframe.toolbars.actiontoolbar.bobbutton -after .mainframe.toolbars.actiontoolbar.deletebutton
+      menu .bobmenu -tearoff false
+      ttk::menubutton .mainframe.toolbars.actiontoolbar.bobbutton -style Toolbutton -image BobIcon -takefocus 0 -menu .bobmenu
       tooltip::tooltip .mainframe.toolbars.actiontoolbar.bobbutton {Show Bob commands menu}
    }
 
@@ -12,7 +12,7 @@ namespace eval bob {
    }
 
    proc on_disable {} {
-      puts {I'm disabled :(}
+      destroy .mainframe.toolbars.actiontoolbar.bobbutton
    }
 
    proc on_start {path} {
@@ -20,6 +20,21 @@ namespace eval bob {
    }
 
    proc on_enter {path} {
-      puts "Entered directory '$path'"
+      set filename [file join $path .bob.yml]
+      if {[file exists $filename] == 0} {
+         pack forget .mainframe.toolbars.actiontoolbar.bobbutton
+         return
+      } else {
+         pack .mainframe.toolbars.actiontoolbar.bobbutton -after .mainframe.toolbars.actiontoolbar.deletebutton
+      }
+      .bobmenu delete 0 end
+      set bobfile [open $filename]
+      while {[gets $bobfile line] >= 0} {
+         if {[string first {name:} $line] == -1} {
+            continue
+         }
+         .bobmenu add command -label [string range $line 9 end]
+      }
+      close $bobfile
    }
 }
