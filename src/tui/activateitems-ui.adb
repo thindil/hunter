f8dd4c649884168af436b5fe584742e9552394cd
thindil <thindil@laeran.pl>
2021-05-11 06:20:56 +0000

    continue work on executing files and directories with the selected application
    FossilOrigin-Name: ec368fc174a88a5181fbe71eca661187d42985b025fdaaadd1a4f55f6bb909e7

--- src/tui/activateitems-ui.adb
+++ src/tui/activateitems-ui.adb
@@ -13,11 +13,60 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Terminal_Interface.Curses.Forms; use Terminal_Interface.Curses.Forms;
+
 package body ActivateItems.UI is
 
+   DialogForm: Forms.Form;
+   FormWindow: Window;
+
    procedure Show_Execute_With_Dialog is
+      Create_Fields: constant Field_Array_Access := new Field_Array(1 .. 5);
+      FormHeight: Line_Position;
+      FormLength: Column_Position;
+      Visibility: Cursor_Visibility := Normal;
+      FieldOptions: Field_Option_Set;
+      UnusedResult: Forms.Driver_Result := Unknown_Request;
    begin
-      null;
+      Set_Cursor_Visibility(Visibility);
+      Create_Fields.all(1) := New_Field(1, 32, 0, 4, 0, 0);
+      Set_Buffer(Create_Fields.all(1), 0, "Enter the application to execute:");
+      FieldOptions := Get_Options(Create_Fields.all(1));
+      FieldOptions.Active := False;
+      Set_Options(Create_Fields.all(1), FieldOptions);
+      Create_Fields.all(2) := New_Field(1, 40, 1, 0, 0, 0);
+      Set_Buffer(Create_Fields.all(2), 0, "");
+      FieldOptions := Get_Options(Create_Fields.all(2));
+      FieldOptions.Auto_Skip := False;
+      Set_Options(Create_Fields.all(2), FieldOptions);
+      Create_Fields.all(3) := New_Field(1, 8, 2, 7, 0, 0);
+      Set_Buffer(Create_Fields.all(3), 0, "[Cancel]");
+      FieldOptions := Get_Options(Create_Fields.all(3));
+      FieldOptions.Edit := False;
+      Set_Options(Create_Fields.all(3), FieldOptions);
+      Create_Fields.all(4) := New_Field(1, 9, 2, 23, 0, 0);
+      FieldOptions := Get_Options(Create_Fields.all(4));
+      FieldOptions.Edit := False;
+      Set_Options(Create_Fields.all(4), FieldOptions);
+      Set_Buffer(Create_Fields.all(4), 0, "[Execute]");
+      Create_Fields.all(5) := Null_Field;
+      DialogForm := New_Form(Create_Fields);
+      Set_Current(DialogForm, Create_Fields(2));
+      Set_Options(DialogForm, (others => False));
+      Scale(DialogForm, FormHeight, FormLength);
+      FormWindow :=
+        Create
+          (FormHeight + 2, FormLength + 2, ((Lines / 3) - (FormHeight / 2)),
+           ((Columns / 2) - (FormLength / 2)));
+      Box(FormWindow, Default_Character, Default_Character);
+      Set_Window(DialogForm, FormWindow);
+      Set_Sub_Window
+        (DialogForm, Derived_Window(FormWindow, FormHeight, FormLength, 1, 1));
+      Post(DialogForm);
+      UnusedResult := Driver(DialogForm, REQ_END_LINE);
+      Refresh;
+      Refresh(FormWindow);
    end Show_Execute_With_Dialog;
 
    function Execute_Form_Keys(Key: Key_Code) return UI_Locations is
