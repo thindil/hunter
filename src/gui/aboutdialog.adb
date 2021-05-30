8029da66a46c72b2d5314c506fe885a9ae0b7d65
thindil <thindil@laeran.pl>
2021-05-30 05:45:02 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: de512c63fcd4bc87745129d6942e02454d32222daf1bfdb4b06a460c93ee1a3f

--- src/gui/aboutdialog.adb
+++ src/gui/aboutdialog.adb
@@ -60,9 +60,7 @@ package body AboutDialog is
    function Show_About_Command
      (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
       Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
-      pragma Unreferenced(Client_Data);
-      pragma Unreferenced(Argc);
-      pragma Unreferenced(Argv);
+      pragma Unreferenced(Client_Data, Argc, Argv);
       About_Dialog: constant Tk_Toplevel :=
         Create(".aboutdialog", "-class Dialog");
       Close_Button: constant Ttk_Button :=
