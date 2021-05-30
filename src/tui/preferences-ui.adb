3685808ff1c17aad829c91a16882e2ebd061c6ed
thindil <thindil@laeran.pl>
2021-05-30 05:45:34 +0000

    continue work on showing the program options
    FossilOrigin-Name: bf73b9761041af21b796bd710fbed2e36b9443df3f529e475ad5648724002f34

--- src/tui/preferences-ui.adb
+++ src/tui/preferences-ui.adb
@@ -13,11 +13,35 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+with Terminal_Interface.Curses.Menus; use Terminal_Interface.Curses.Menus;
+
 package body Preferences.UI is
 
+   OptionsMenu: Menu;
+   MenuWindow: Window;
+
    procedure Show_Options is
+      Main_Menu_Array: constant array(1 .. 5) of Unbounded_String :=
+        (To_Unbounded_String("Preferences"), To_Unbounded_String("Shortcuts"),
+         To_Unbounded_String("Commands"), To_Unbounded_String("Modules"),
+         To_Unbounded_String("Close"));
+      Menu_Items: constant Item_Array_Access := new Item_Array(1 .. 6);
    begin
-      null;
+      Clear;
+      Create_Program_Menu_Loop :
+      for I in Main_Menu_Array'Range loop
+         Menu_Items.all(I) := New_Item(To_String(Main_Menu_Array(I)));
+      end loop Create_Program_Menu_Loop;
+      Menu_Items.all(6) := Null_Item;
+      OptionsMenu := New_Menu(Menu_Items);
+      Set_Format(OptionsMenu, 1, 5);
+      Set_Mark(OptionsMenu, "");
+      MenuWindow := Create(1, Columns, 0, 0);
+      Set_Window(OptionsMenu, MenuWindow);
+      Set_Sub_Window
+        (OptionsMenu, Derived_Window(MenuWindow, 1, Columns, 0, 0));
+      Post(OptionsMenu);
+      Refresh(MenuWindow);
    end Show_Options;
 
    function Preferences_Keys(Key: Key_Code) return UI_Locations is
