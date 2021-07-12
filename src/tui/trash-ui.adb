1538d7be2b62817f91fdc565f12fba082dd9481d
thindil <thindil@laeran.pl>
2021-07-12 05:35:04 +0000

    started work on clearing Trash action in console version
    FossilOrigin-Name: df71698eb9c9d1423e80ece386f9bebf43d61b385f650ef22334c60d55fcf8da

---
+++ src/tui/trash-ui.adb
@@ -0,0 +1,53 @@
+-- Copyright (c) 2019-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Calendar.Formatting; use Ada.Calendar.Formatting;
+with Ada.Containers; use Ada.Containers;
+with Ada.Directories; use Ada.Directories;
+with Ada.Environment_Variables; use Ada.Environment_Variables;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_IO; use Ada.Text_IO;
+with Interfaces.C.Strings; use Interfaces.C.Strings;
+with GNAT.Directory_Operations; use GNAT.Directory_Operations;
+with GNAT.OS_Lib; use GNAT.OS_Lib;
+with Tcl.Tk.Ada.Pack;
+with Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
+use Tcl.Tk.Ada.Widgets.Toplevel.MainWindow;
+with Tcl.Tk.Ada.Widgets.TtkButton; use Tcl.Tk.Ada.Widgets.TtkButton;
+with Inotify; use Inotify;
+with LoadData; use LoadData;
+with MainWindow; use MainWindow;
+with Modules; use Modules;
+with Preferences; use Preferences;
+with ShowItems; use ShowItems;
+with Utils; use Utils;
+with Utils.UI; use Utils.UI;
+
+package body Trash.UI is
+
+   function Show_Trash_Command
+     (ClientData: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int is
+      pragma Unreferenced(ClientData, Interp, Argc, Argv);
+   begin
+      return TCL_OK; 
+   end Show_Trash_Command;
+
+   procedure CreateTrashUI is
+   begin
+      Add_Command("ShowTrash", Show_Trash_Command'Access);
+   end CreateTrashUI;
+
+end Trash.UI;
---
+++ src/tui/trash-ui.ads
@@ -0,0 +1,51 @@
+-- Copyright (c) 2019-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Interfaces.C;
+with CArgv;
+with Tcl; use Tcl;
+
+-- ****h* Trash/TUI
+-- FUNCTION
+-- Provide code to manipulate system Trash
+-- SOURCE
+package Trash.UI is
+-- ****
+
+   -- ****o* TUI/TUI.Show_Trash_Command
+   -- FUNCTION
+   -- Show content of the Trash
+   -- PARAMETERS
+   -- ClientData - Custom data send to the command. Unused
+   -- Interp     - Tcl interpreter in which command was executed.
+   -- Argc       - Number of arguments passed to the command. Unused
+   -- Argv       - Values of arguments passed to the command. Unused
+   -- COMMANDS
+   -- ShowTrash
+   -- SOURCE
+   function Show_Trash_Command
+     (ClientData: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+      Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
+      Convention => C;
+      -- ****
+
+   -- ****f* Trash/TUI.CreateTrashUI
+   -- FUNCTION
+   -- Create trash UI
+   -- SOURCE
+   procedure CreateTrashUI;
+   -- ****
+
+end Trash.UI;
