8edce139f86dd217e3117dc441bb23a637b869ef
thindil <thindil@laeran.pl>
2021-08-05 05:21:21 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: b249376632c03f77ca3fba0ffa005aad966aaebcbba6218a1adc2a1cd7a206c2

--- src/loaddata.ads
+++ src/loaddata.ads
@@ -29,57 +29,73 @@ package LoadData is
    -- FUNCTION
    -- Used to store size of files and directories
    -- SOURCE
-   type Item_Size is range -2 .. File_Size'Last;
+   type Item_Size is range -2 .. File_Size'Last with
+      Default_Value => 0;
+      -- ****
+
+      -- ****d* LoadData/LoadData.Empty_Item_Size
+      -- FUNCTION
+      -- The size of empty item (file or directory)
+      -- SOURCE
+   Empty_Item_Size: constant Item_Size := 0;
    -- ****
 
    -- ****s* LoadData/LoadData.Item_Record
    -- FUNCTION
    -- Data structure with information about files and directories
    -- OPTIONS
-   -- Name        - The name of the file or directory
-   -- Size        - For file size in bytes, for directory amount of items
-   --               inside
-   -- IsDirectory - If True, the item is a directory, otherwise false
-   -- IsHidden    - If True, the item is hidden, otherwise false
-   -- Modified    - Time of last modificiation of the item
-   -- Image       - The name of the image used to show on the left to the
-   --               item on the list
-   -- HiddenItems - In directory, amount of hidden items
-   -- Path        - The path to the selected item. Used in showing Trash
-   --               content
+   -- Name         - The name of the file or directory
+   -- Size         - For file size in bytes, for directory amount of items
+   --                inside
+   -- Is_Directory - If True, the item is a directory, otherwise false
+   -- Is_Hidden    - If True, the item is hidden, otherwise false
+   -- Modified     - Time of last modificiation of the item
+   -- Image        - The name of the image used to show on the left to the
+   --                item on the list
+   -- Hidden_Items - In directory, amount of hidden items
+   -- Path         - The path to the selected item. Used in showing Trash
+   --                content
    -- SOURCE
    type Item_Record is record
       Name: Unbounded_String;
       Size: Item_Size;
-      IsDirectory: Boolean;
-      IsHidden: Boolean;
+      Is_Directory: Boolean;
+      Is_Hidden: Boolean;
       Modified: Time;
       Image: Unbounded_String;
-      HiddenItems: Natural;
+      Hidden_Items: Natural;
       Path: Unbounded_String;
    end record;
    -- ****
 
-   -- ****t* LoadData/LoadData.SortingOrder
+   -- ****t* LoadData/LoadData.Sorting_Order
    -- FUNCTION
    -- Possible options of sorting for items in directory view
    -- OPTIONS
-   -- NameAsc      - Sort by name ascending
-   -- NameDesc     - Sort by name descending
-   -- ModifiedAsc  - Sort by modified time ascending
-   -- ModifiedDesc - Sort by modified time descending
-   -- SizeAsc      - Sort by size ascending
-   -- SizeDesc     - Sort by size descending
+   -- NAMEASC      - Sort by name ascending
+   -- NAMEDESC     - Sort by name descending
+   -- MODIFIEDASC  - Sort by modified time ascending
+   -- MODIFIEDDESC - Sort by modified time descending
+   -- SIZEASC      - Sort by size ascending
+   -- SIZEDESC     - Sort by size descending
    -- SOURCE
-   type SortingOrder is
-     (NameAsc, NameDesc, ModifiedAsc, ModifiedDesc, SizeAsc, SizeDesc);
+   type Sorting_Order is
+     (NAMEASC, NAMEDESC, MODIFIEDASC, MODIFIEDDESC, SIZEASC, SIZEDESC) with
+      Default_Value => NAMEASC;
+     -- ****
+
+     -- ****d* LoadData/LoadData.Default_Sorting_Order
+     -- FUNCTION
+     -- Default sorting order for directory view
+     -- SOURCE
+   Default_Sorting_Order: constant Sorting_Order := NAMEASC;
    -- ****
 
-   -- ****v* LoadData/LoadData.SortOrder
+   -- ****v* LoadData/LoadData.Sort_Order
    -- FUNCTION
    -- Currently set sorting order for directory view. Default value is NameAsc
    -- SOURCE
-   SortOrder: SortingOrder := NameAsc;
+   Sort_Order: Sorting_Order := Default_Sorting_Order;
    -- ****
 
    -- ****f* LoadData/LoadData."<"
@@ -110,11 +126,19 @@ package LoadData is
    overriding function "="(Left, Right: Item_Record) return Boolean;
    -- ****
 
+   -- ****d* LoadData/LoadData.Empty_Item
+   -- FUNCTION
+   -- Empty item data
+   -- SOURCE
+   Empty_Item: constant Item_Record := (others => <>);
+   -- ****
+
    -- ****t* LoadData/LoadData.Items_Container
    -- FUNCTION
    -- Used to store information about selected directories contents
    -- SOURCE
-   package Items_Container is new Vectors(Positive, Item_Record);
+   package Items_Container is new Vectors
+     (Index_Type => Positive, Element_Type => Item_Record);
    -- ****
 
    -- ****t* LoadData/LoadData.Items_Sorting
@@ -124,29 +148,29 @@ package LoadData is
    package Items_Sorting is new Items_Container.Generic_Sorting;
    -- ****
 
-   -- ****v* LoadData/LoadData.ItemsList
+   -- ****v* LoadData/LoadData.Items_List
    -- FUNCTION
    -- The content of the currently viewed directory
    -- SOURCE
-   ItemsList: Items_Container.Vector;
+   Items_List: Items_Container.Vector;
    -- ****
 
-   -- ****v* LoadData/LoadData.SecondItemsList
+   -- ****v* LoadData/LoadData.Second_Items_List
    -- FUNCTION
    -- The content of the currently previewed directory or destination
    -- directory for actions like moving, copying, creating links
    -- SOURCE
-   SecondItemsList: Items_Container.Vector;
+   Second_Items_List: Items_Container.Vector;
    -- ****
 
-   -- ****f* LoadData/LoadData.AddItem
+   -- ****f* LoadData/LoadData.Add_Item
    -- FUNCTION
    -- Add the selected item to the selected list
    -- PARAMETERS
    -- Path - Full path to the file or directory which will be added
    -- List - The list to which a new item will be added
    -- SOURCE
-   procedure AddItem(Path: String; List: in out Items_Container.Vector);
+   procedure Add_Item(Path: String; List: in out Items_Container.Vector);
    -- ****
 
 end LoadData;
