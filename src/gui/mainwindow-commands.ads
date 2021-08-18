83e2c25d245ff7c946a3c9d5452aa9656b856fcb
thindil <thindil@laeran.pl>
2021-08-18 04:50:49 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: 8cc2279e0ca19073d6ba3f72e84ba71f5ffce91f94c751c19167b1a0c25f66df

--- src/gui/mainwindow-commands.ads
+++ src/gui/mainwindow-commands.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2020-2021 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package MainWindow.Commands is
 -- ****
 
-   -- ****f* MCommands/MCommands.AddCommands
+   -- ****f* MCommands/MCommands.Add_Commands
    -- FUNCTION
    -- Adds the Ada code to the Tcl interpreter
    -- SOURCE
-   procedure AddCommands;
+   procedure Add_Commands;
    -- ****
 
 end MainWindow.Commands;
