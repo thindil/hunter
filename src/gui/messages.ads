a712b2caf480d32eff1f334e6164b62d4b51f3c9
thindil <thindil@laeran.pl>
2021-09-16 05:42:48 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: a6016ed14df939808a2a1659d9e1f8c4aeaa871faa852d5c782997ef852a6d88

--- src/gui/messages.ads
+++ src/gui/messages.ads
@@ -24,49 +24,49 @@ with Tcl; use Tcl;
 package Messages is
 -- ****
 
-   -- ****v* Messages/Messages.YesToAll
+   -- ****v* Messages/Messages.Yes_For_All
    -- FUNCTION
    -- Set to True if user clicked Yes for All button in response to question,
    -- otherwise False
    -- SOURCE
-   YesForAll: Boolean;
+   Yes_For_All: Boolean;
    -- ****
 
    -- ****o* Messages/Messages.Close_Command
    -- FUNCTION
    -- Hide message frame
    -- PARAMETERS
-   -- ClientData - Custom data send to the command. Unused
-   -- Interp     - Tcl interpreter in which command was executed. Unused
-   -- Argc       - Number of arguments passed to the command. Unused
-   -- Argv       - Values of arguments passed to the command. Unused
+   -- Client_Data - Custom data send to the command. Unused
+   -- Interp      - Tcl interpreter in which command was executed. Unused
+   -- Argc        - Number of arguments passed to the command. Unused
+   -- Argv        - Values of arguments passed to the command. Unused
    -- RESULT
    -- This function always return TCL_OK
    -- COMMANDS
    -- CloseMessage
    -- SOURCE
    function Close_Command
-     (ClientData: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
+     (Client_Data: Integer; Interp: Tcl.Tcl_Interp; Argc: Interfaces.C.int;
       Argv: CArgv.Chars_Ptr_Ptr) return Interfaces.C.int with
       Convention => C;
       -- ****
 
-      -- ****f* Messages/Messages.CreateMessagesUI
+      -- ****f* Messages/Messages.Create_Messages_Ui
       -- FUNCTION
       -- Create UI related to the program messages
       -- SOURCE
-   procedure CreateMessagesUI;
+   procedure Create_Messages_Ui;
    -- ****
 
-   -- ****f* Messages/Messages.ShowMessage
+   -- ****f* Messages/Messages.Show_Message
    -- FUNCTION
    -- Show selected message with selected type to the user
    -- PARAMETERS
-   -- Message     - Text of message to show to the user
-   -- MessageType - Type of message. Posible values are error, message,
-   --               question. Default value is error.
+   -- Message      - Text of message to show to the user
+   -- Message_Type - Type of message. Posible values are error, message,
+   --                question. Default value is error.
    -- SOURCE
-   procedure ShowMessage(Message: String; MessageType: String := "error");
+   procedure Show_Message(Message: String; Message_Type: String := "error");
    -- ****
 
 end Messages;
