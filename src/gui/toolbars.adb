aff93c448a93c8c9dc93176d3dab35d7c42b388e
thindil <thindil@laeran.pl>
2022-01-16 04:47:21 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 9e67704879c81e903e8378a511bbe833f67a224a953482648aa5aa37c32b10f3

--- src/gui/toolbars.adb
+++ src/gui/toolbars.adb
@@ -114,10 +114,10 @@ package body Toolbars is
       end loop Set_Info_Separators_Loop;
       Toolbar.Interp := Get_Context;
       Toolbar.Name := New_String(Str => Main_Frame & ".toolbars.itemtoolbar");
-      if not Settings.Toolbars_On_Top then
-         Grid_Configure(Toolbar, "-column 0 -row 2 -sticky s");
-      else
+      if Settings.Toolbars_On_Top then
          Grid_Configure(Toolbar, "-column 2 -row 0 -sticky e");
+      else
+         Grid_Configure(Toolbar, "-column 0 -row 2 -sticky s");
       end if;
       Toolbar.Name := New_String(Main_Frame & ".toolbars");
       if not Settings.Toolbars_On_Top then
