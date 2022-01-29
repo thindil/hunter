6f1cda79fde5fb7125eba0966fe754c616d7b214
thindil <thindil@laeran.pl>
2022-01-29 04:44:01 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 7e9c598ce8223cebcc779bcdb89434b177644b410d47f6d3f977e73b9df4f437

--- src/trash.ads
+++ src/trash.ads
@@ -1,4 +1,4 @@
--- Copyright (c) 2019-2021 Bartek thindil Jasicki <thindil@laeran.pl>
+-- Copyright (c) 2019-2022 Bartek thindil Jasicki <thindil@laeran.pl>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@
 package Trash is
 -- ****
 
-   -- ****f* Trash/Trash.CreateTrash
+   -- ****f* Trash/Trash.Create_Trash
    -- FUNCTION
    -- Create trash related Tcl commands
    -- SOURCE
-   procedure CreateTrash;
+   procedure Create_Trash;
    -- ****
 
 private
