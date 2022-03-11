ab156b13f0055a38b014a361fd2749e484488d99
thindil <thindil@laeran.pl>
2022-03-11 04:37:42 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: e76efa1c24bbe1a4f45e43ae27c5be02260ec8cf5e2ee67bdb16f0c35325e032

--- src/preferences.adb
+++ src/preferences.adb
@@ -234,9 +234,11 @@ package body Preferences is
 
       Config_File: File_Type;
       Configuration: DOM_Implementation; --## rule line off IMPROPER_INITIALIZATION
-      Main_Node: DOM.Core.Element; --## rule line off GLOBAL_REFERENCES
-      Setting_Node: DOM.Core.Element;
+      --## rule off GLOBAL_REFERENCES
+      Main_Node: DOM.Core.Element;
       Save_Settings_Data: Document;
+      --## rule on GLOBAL_REFERENCES
+      Setting_Node: DOM.Core.Element;
       Unused_Node: Text;
       procedure Save_Boolean(Value: Boolean; Name: String) is
       begin
