f580792aff24efcd55d7b6796748b79e05271894
thindil <thindil@laeran.pl>
2022-03-29 04:00:49 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 9033878cf0af5883191fd09bb61d0515a26e8c5ddbf3a5652188b86c0d343988

--- src/programsmenu.ads
+++ src/programsmenu.ads
@@ -13,7 +13,9 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+--## rule off REDUCEABLE_SCOPE
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+--## rule on REDUCEABLE_SCOPE
 with Bookmarks; use Bookmarks;
 with CopyItems; use CopyItems;
 
