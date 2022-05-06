1476fe9d54e28fa8417f9c2537f65237718bbf58
thindil <thindil@laeran.pl>
2022-05-06 03:43:49 +0000

    feat: started work on keyboard shortcuts in the console version
    FossilOrigin-Name: 46e2a6608f3181d29085b427f1ca7c2daf0cab455ff9ba95c39f92fb15ded8fa

---
+++ src/tui/shortcuts.adb
@@ -0,0 +1,50 @@
+-- Copyright (c) 2022 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+package body RenameItems.UI is
+
+   function Shortcut_Keys(Key: Key_Code; AltKey: Boolean; Old_Location: UI_Locations) return UI_Locations is
+      Key_Value: constant String := Key_Name(Key);
+      New_Key: Unbounded_String := Null_Unbounded_String;
+      Index: Natural := 0;
+   begin
+      if AltKey then
+         New_Key := To_Unbounded_String("Alt-" & Key_Value);
+      else
+         if Key_Value(Key_Value'First) /= '^' then
+            New_Key := To_Unbounded_String(Key_Value);
+         else
+            New_Key :=
+              To_Unbounded_String
+                ("Control-" & To_Lower(Key_Value(Key_Value'Last)));
+         end if;
+      end if;
+      for I in Accelerators'Range loop
+         if Accelerators(I) = New_Key then
+            Index := I;
+            break;
+         end if;
+         case Index is
+            when 1 =>
+            when others =>
+               null;
+         end case;
+      end loop;
+      return Old_Location;
+   end Rename_Keys;
+
+end RenameItems.UI;
---
+++ src/tui/shortcuts.ads
@@ -0,0 +1,40 @@
+-- Copyright (c) 2022 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Terminal_Interface.Curses; use Terminal_Interface.Curses;
+with MainWindow; use MainWindow;
+
+-- ****h* Shortcuts/Shortcuts
+-- FUNCTION
+-- Provide code to handle keyboard shortcuts
+-- SOURCE
+package Shortcuts is
+-- ****
+
+   -- ****f* Shortcuts/Shortcuts.Shortcuts_Keys
+   -- FUNCTION
+   -- Handles keyboard shortcuts in console version
+   -- PARAMETERS
+   -- Key           - Key pressed by the user
+   -- AltKey        - If True, ALT key was pressed too
+   -- Old_Localtion - The currently selected UI element
+   -- RESULT
+   -- The currently selected UI element of the program
+   -- SOURCE
+   function Shortcuts_Keys
+     (Key: Key_Code; AltKey: Boolean; Old_Location: UI_Locations) return UI_Locations;
+   -- ****
+
+end Shortcuts;
